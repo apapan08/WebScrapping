3f1dc44ea6741c1fd2c2bb5cde723dcbc89bf4a6
Angelos Papanastasiou <aggelospapanastasiou905@gmail.com>
2023-03-19 22:46:09 +0200

    initial Latest year

--- webscrapping.R
+++ webscrapping.R
@@ -8,7 +8,7 @@ Sys.setlocale(category = "LC_ALL", locale = "Greek")
 '%!in%' <- function(x,y)!('%in%'(x,y))
 # 1. Extract the updated league table  
 
-TableLink <- "https://www.cfa.com.cy/Gr/competitions/33715608"
+TableLink <- "https://cfa.com.cy/Gr/competitions/50549072"
 
 page <- read_html(TableLink)
 
@@ -20,7 +20,7 @@ colnames(LeaugeTable) <- c("POSITION","TEAM","GAMES","W","D","L","GF","GA","PTS"
 
 # 2. EXTRACT ALL MATCH RESULTS
 
-ScoreLink <- "https://www.cfa.com.cy/Gr/fixtures/33715608"
+ScoreLink <- "https://cfa.com.cy/Gr/fixtures/%2050549072"
 
 page <- read_html(ScoreLink)
 
@@ -30,30 +30,34 @@ ScoreTable <- as.data.table(
 
 ScoreTable <- ScoreTable[,.(`Home Team`= X,Score = X.2,`Away Team` = X.4, Stadium = X.5)]
 
-#Note : orfeas - troulloi : 3 - 0 decision of kop
+#Notes (decisions of kop) : 
+# Thiella - mouttagiakka : 3 - 0 
+# Pseudas AEN 0-3
+# Orfeas - Geroskipou 3 - 0
+
 
 # 2a Fix Tables
 
+# Manually fix cancelled matches 
+
+ScoreTable[,k:=1:.N]
 
 
-#apea vs aspis was cancelled and played on the next day so we delete it
-ScoreTable <-  ScoreTable[!(Score=="14:30")]
+ScoreTable <- ScoreTable[!((k < 197) & Score %in% c("15:00","14:30","15:30"))]
 
+ScoreTable[,k := NULL]
 
-number_of_matches<-11
+number_of_matches <- 15
 
-start_date <- lubridate::dmy("25/9/2021")
+start_date <- lubridate::dmy("24/9/2022")
 
-allpossible_dates<-seq(start_date, lubridate::dmy("02/04/2022"), by=7)
+allpossible_dates <- seq(start_date, lubridate::dmy("06/05/2023"), by=7)
 
 
 # Manually exclude dates with no games
-to_exclude_dates <- c(lubridate::dmy("13/11/2021"),
-                      lubridate::dmy("09/10/2021"),
-                      lubridate::dmy("25/12/2021"),
-                      lubridate::dmy("01/01/2022"),
-                      lubridate::dmy("05/03/2022"),
-                      lubridate::dmy("26/03/2022"))
+to_exclude_dates <- c(lubridate::dmy("24/12/2022"),
+                      lubridate::dmy("31/12/2022"),
+                      lubridate::dmy("15/04/2023"))
 
 #create dates dim
 allpossible_dates = allpossible_dates[which(allpossible_dates %!in% to_exclude_dates)]
@@ -67,7 +71,7 @@ date_mapping<-data.table(
 
 #The data are extracted with order so . .
 
-ScoreTable[,`Match day`:= unlist(purrr::map(1:(number_of_matches*2),~rep(.x,6)))]
+ScoreTable[,`Match day`:= unlist(purrr::map(1:(number_of_matches*2),~rep(.x,8)))]
 
 #make score variables
 
@@ -93,136 +97,76 @@ ScoreTable <- date_mapping[ScoreTable,on=.(`Match day`)]
 ScoreTable[, (c("Score Away","Score Home")) := lapply(.SD,as.integer), .SDcols = c("Score Away","Score Home")]
 
 
-
-# 3. Extract info players
-
-page <- read_html(TableLink)
+# Add Result 
 
 
-# extract link for each team 
-AllLinks <- page%>%html_nodes(".std-team-text")
-
-MappingTeamTable <- data.table(
-  Team = LeaugeTable[,TEAM],
-  Link = sapply(AllLinks, function(x) xml_attrs(x)[["href"]])
-)
-
-# extract all players
-PlayerTable <- foreach(i=1:nrow(MappingTeamTable)) %do% {
-  PageTeamLink = paste0("https://www.cfa.com.cy",MappingTeamTable[i,Link])
-  PageTeamHtml <- read_html(PageTeamLink)
-  TeamTable <- as.data.table(PageTeamHtml %>% html_nodes("table.col-md-12")%>%
-                               html_table(header = T))
-  TeamTable[,TEAM := MappingTeamTable[i,Team]]
-  colnames(TeamTable) = c("NAME","APPS","SUB","STARTING","GOAL","OWN GOAL","YELLOW","RED","MINUTES","TEAM")
-  return(TeamTable)
-}
-
-PlayerTable <- rbindlist(PlayerTable)
+ScoreTable[, Result := ifelse(`Score Home` > `Score Away`,1,ifelse(`Score Away` > `Score Home`,2,"x"))]
 
 
+# Calculate  current standings 
 
-
-#function to extract Age for each player
-CheckIfHref <- function(x) {
-  if ("href" %in% names(xml_attrs(x))){
-    TRUE
-  } else {
-    FALSE
-  }
-}
-
-#make it faster with do par 
-PlayerAgeInfo <- foreach(i=1:nrow(MappingTeamTable)) %do% {
-  PageTeamLink = paste0("https://www.cfa.com.cy",MappingTeamTable[i,Link])
-  PlayerInfoHtml <- read_html(PageTeamLink) %>% html_nodes("a")%>%html_attr("href")
+# Function to calculate league standings
+calculate_standings <- function(data) {
+  standings <- data.table(Team = unique(c(data$`Home Team`, data$`Away Team`)),
+                          Points = 0, Wins = 0, Draws = 0, Losses = 0)
   
-  
-  AllPlayersLink <- unique(PlayerInfoHtml[grepl("player",PlayerInfoHtml)])
-  PlayersEachTeam <- foreach(j=1:length(AllPlayersLink))%do% {
-    playerlink = AllPlayersLink[j]
-    temp_info <- read_html(paste0("https://www.cfa.com.cy",playerlink)) %>%
-      html_nodes("td.rightColumn")%>%html_text()
-    temp_name <-read_html(paste0("https://www.cfa.com.cy",playerlink)) %>%
-      html_nodes(".fpHeader1 span")%>%html_text()
-    dt_temp <- data.table(
-      Team = temp_info[1],
-      Name = temp_name,
-      DateBirth = temp_info[2]
-    )
-    return(dt_temp)
-  }
-  PlayersEachTeam <- rbindlist(PlayersEachTeam)
-  return(PlayersEachTeam)
-}
-
-
-PlayerAgeInfo <- rbindlist(PlayerAgeInfo)
-
-#For some reason only one player has its position in the place where the age should have been 
-
-PlayerAgeInfo <- PlayerAgeInfo[Name == "SHEVERINIO SHENILLIO  BLANKER ",DateBirth :="29/05/1999"]
-
-PlayerAgeInfo[,Age := floor(interval(lubridate::dmy(DateBirth), lubridate::ymd("2022-03-30")) / years(1))]
-
-
-# 4. Extract info matches
-page <- read_html(ScoreLink)
-AllLinksInfo <- unique(
-  page %>% html_nodes("a")%>%html_attr("href")
-)
-
-AllGamesLink <- unique(AllLinksInfo[grepl("game_details",AllLinksInfo)])
-
-#Manually remove APEA-ASPIS match
-
-AllGamesLink <- AllGamesLink[!(grepl("33742188",AllGamesLink))]
-
-MatchIdMapping = data.table(
-  matchid = 1:length(AllGamesLink),
-  link = AllGamesLink
-)
-
-
-AllmatchGoals <- foreach(i =  1:length(AllGamesLink))%do% {
-  testing_link <- AllGamesLink[i]
-  GameInfoPage <- read_html(paste0("https://www.cfa.com.cy",testing_link))
-  #check if any goals
-  AnyGoals <- any(sapply(GameInfoPage %>%html_nodes("img"), function(x) grepl("ball.gif",x)))
-  if (AnyGoals){
-    InfoGameTable <- as.data.table(GameInfoPage %>% html_nodes("table:nth-of-type(3)")%>%html_table())
-    InfoGameTable[,section_id := ifelse(((X1==""|is.na(X1)))&(X2==""|is.na(X2))&(X3==""|is.na(X3)),1,0)]
-    InfoGameTable[,section_id := cumsum(section_id)]
-    InfoGameTable = InfoGameTable[(section_id<=1)&((X1!="")|(X2!=""))]
-    MinutesScored = data.table(
-      MatchId = MatchIdMapping[i,matchid],
-      HomeGoals = list(
-        sapply(InfoGameTable[X1 != "",X2],function(x) as.numeric(stringi::stri_replace_all_fixed(x,"'","")),USE.NAMES = FALSE)),
-      AwayGoals = list(
-        sapply(InfoGameTable[X3 != "",X2],function(x) as.numeric(stringi::stri_replace_all_fixed(x,"'","")),USE.NAMES = FALSE)
-      )
-    )
-    return(MinutesScored)
+  for (i in 1:nrow(data)) {
+    match <- data[i]
+    team_a <- match$`Home Team`
+    team_b <- match$`Away Team`
+    result <- match$Result
     
-  }else{
-    MinutesScored = data.table(
-      MatchId = MatchIdMapping[i,matchid],
-      HomeGoals = numeric(),
-      AwayGoals = numeric()
-    )
-    return(MinutesScored)
+    team_a_index <- which(standings$Team == team_a)
+    team_b_index <- which(standings$Team == team_b)
+    
+    if (result == "1") {
+      standings[team_a_index, c("Points", "Wins") := list(Points + 3, Wins + 1)]
+      standings[team_b_index, c("Losses") := Losses + 1]
+    } else if (result == "2") {
+      standings[team_b_index, c("Points", "Wins") := list(Points + 3, Wins + 1)]
+      standings[team_a_index, c("Losses") := Losses + 1]
+    } else if (result == "x") {
+      standings[team_a_index, c("Points", "Draws") := list(Points + 1, Draws + 1)]
+      standings[team_b_index, c("Points", "Draws") := list(Points + 1, Draws + 1)]
+    }
   }
+  
+  standings <- standings[order(-Points, -Wins, -Draws, Losses)]
+  return(standings)
 }
-AllmatchGoals <- rbindlist(AllmatchGoals)
-# MATCHID
-ScoreTable[,MatchId := 1:.N]
 
-ScoreTable <- AllmatchGoals[ScoreTable,on = .(MatchId)]
+standings <- calculate_standings(ScoreTable[!is.na(Result)])
 
-# We output four files 
+ScoreTable[, MatchDayId := 1:.N , by = .(`Match day`)]
+
+ScoreTable[, MatchIDUnique := paste0(`Match day`,"_",MatchDayId)]
+
+current_match_day <- 24
+
+matches <- ScoreTable[`Match day` == current_match_day + 1,.(MatchIDUnique)]
+
+# Recursive function to generate all possible outcomes
+generate_outcomes <- function(matches, outcomes = list()) {
+    current_match <- matches[1]
+    remaining_matches <- matches[-1]
+    new_outcomes <- list()
+    
+    for (result in c("1", "x", "2")) {
+      current_outcome <- copy(current_match)
+      current_outcome$Result <- result
+      
+      if (length(outcomes) == 0) {
+        new_outcomes <- c(new_outcomes, list(current_outcome))
+      } else {
+        for (outcome in outcomes) {
+          new_outcomes <- c(new_outcomes, list(rbind(outcome, current_outcome)))
+        }
+      }
+    }
+    
+    return(generate_outcomes(remaining_matches, new_outcomes))
+}
 
 
-saveRDS(ScoreTable,"output/Scoretable.rds")
-saveRDS(LeaugeTable,"output/LeaugeTable.rds")
-saveRDS(PlayerAgeInfo,"output/PlayerAgeInfo.rds")
+AllOutcomes <- generate_outcomes(matches = matches)
 
